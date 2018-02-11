e924d556846b1aa2d4fdb6bb0c7825d269e01ead
DarthThanatos <robert.bielas@interia.pl>
2018-02-11 13:00:58 +0100

    imagepanel resizing

--- src/display/ImagePanel.java
+++ src/display/ImagePanel.java
@@ -2,20 +2,55 @@ package display;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-public class ImagePanel extends JPanel{
+public class ImagePanel extends JPanel implements ComponentListener{
 
+    private JLabel picLabel;
+    private BufferedImage image;
+    private static final int BORDER_WIDTH = 3;
 
     public ImagePanel(String imagePath){
         try{
-            BufferedImage image = ImageIO.read(new File("src/" + imagePath));
-            JLabel picLabel = new JLabel(new ImageIcon(image.getScaledInstance(300, 175, 0)));
+            image = ImageIO.read(new File("src/" + imagePath));
+            addComponentListener(this);
+            picLabel = new JLabel(new ImageIcon(image));
             add(picLabel);
+
         }
         catch(Exception e){
             e.printStackTrace();
         }
     }
+
+
+
+    private void resizeImage(){
+        if(getWidth() == 0 || getHeight() == 0) return;
+        picLabel .setIcon(new ImageIcon(image.getScaledInstance(getWidth() - BORDER_WIDTH, getHeight() - BORDER_WIDTH, 0)));
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        resizeImage();
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
+    }
+
 }
